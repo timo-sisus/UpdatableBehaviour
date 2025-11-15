264a1058bac9f6b95aff96993966d40937403201
Timo Naskali <45103521+timo-sisus@users.noreply.github.com>
2025-11-15 22:59:33 +0200

    Optimizations
    - Use curiously recurring generic pattern to de-virtualize OnUpdate calls as a performance optimization.
    - OnDisable optimized to just swap the removed item with the last item, when possible.

--- UpdatableBehaviour.cs
+++ UpdatableBehaviour.cs
@@ -1,103 +1,159 @@
 using System;
+using System.Linq;
+using System.Reflection;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.LowLevel;
 using UnityEngine.PlayerLoop;
+using UnityEngine.Scripting;
 
 namespace Sisus
 {
 	/// <summary>
-	/// A base class for components with an OnUpdate method that gets executed every frame.
+	/// A base class for components with an <see cref="OnUpdate"/> method that gets executed every frame.
 	/// </summary>
+	/// <typeparam name="T"> Type of the derived class. </typeparam>
 	/// <remarks>
 	/// <see cref="OnUpdate"/> is considerably more performant than MonoBehaviour.Update when you have many instances.
 	/// </remarks>
-	public abstract class UpdatableBehaviour : MonoBehaviour
+	public abstract class UpdatableBehaviour<T> : UpdatableBehaviour where T : UpdatableBehaviour<T>
 	{
 		const int InitialCapacity = 32;
-		static UpdatableBehaviour[] instances = new UpdatableBehaviour[InitialCapacity];
+		static T[] instances = new T[InitialCapacity];
 		static int instanceCount;
 		static int currentInstanceIndex;
 
-		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-		static void StartUpdatingAllInstances()
-		{
-			Debug.Assert(instanceCount is 0, instanceCount);
-			var playerLoop = PlayerLoop.GetCurrentPlayerLoop();
-			var rootSystems = playerLoop.subSystemList;
-			for(var i = rootSystems.Length - 1; i >= 0; i--)
-			{
-				var rootSystem = rootSystems[i];
-				if(rootSystem.type != typeof(Update))
-				{
-					continue;
-				}
-				
-				var oldUpdateSystems = rootSystem.subSystemList;
-				if(Array.FindIndex(oldUpdateSystems, x => x.type == typeof(UpdatableBehaviour)) is not -1)
-				{
-					return;
-				}
-
-				var oldLength = oldUpdateSystems.Length;
-				var newLength = oldLength + 1;
-				var newUpdateSystems = new PlayerLoopSystem[newLength];
-				Array.Copy(oldUpdateSystems, newUpdateSystems, oldLength);
-				newUpdateSystems[oldLength] = new()
-				{
-					type = typeof(UpdatableBehaviour),
-					updateDelegate = UpdateAllInstances
-				};
-				rootSystem.subSystemList = newUpdateSystems;
-				rootSystems[i] = rootSystem;
-				playerLoop.subSystemList = rootSystems;
-				PlayerLoop.SetPlayerLoop(playerLoop);
-				return;
-			}
-
-			static void UpdateAllInstances()
-			{
-				for(currentInstanceIndex = instanceCount - 1; currentInstanceIndex >= 0; currentInstanceIndex--)
-				{
-					instances[currentInstanceIndex].OnUpdate();
-				}
-			}
-		}
-
-		protected abstract void OnUpdate();
+		int instanceIndex = -1;
 
 		void OnEnable()
 		{
-			instanceCount++;
 			if(instanceCount >= instances.Length)
 			{
 				var capacity = instanceCount + instanceCount;
 				Array.Resize(ref instances, capacity);
 			}
 
-			instances[instanceCount - 1] = this;
+			instanceIndex = instanceCount;
+			instances[instanceCount] = (T)this;
+			instanceCount++;
 		}
 
 		void OnDisable()
 		{
-			var removeAt = Array.IndexOf(instances, this, 0, instanceCount);
-			if(removeAt is -1)
+			instanceCount--;
+
+			// If we're removing the last item, we can just set it to null.
+			if(instanceIndex >= instanceCount)
 			{
+				instances[instanceIndex] = null;
 				return;
 			}
 
-			instances[removeAt] = null;
-			instanceCount--;
-			if(removeAt >= instanceCount)
+			// If UpdateAllInstances is currently not executing, or if we're removing
+			// an item after the current index, we can use a faster removal method,
+			// where we just move the last item into the removed item's slot.
+			if(currentInstanceIndex <= instanceIndex)
 			{
+				var lastItem = instances[instanceCount];
+				instances[instanceIndex] = lastItem;
+				lastItem.instanceIndex = instanceIndex;
+				instances[instanceCount] = null;
 				return;
 			}
+
+			// If UpdateAllInstances is currently executing, and the removed item is before
+			// the current index, then we need to use a slower method that ensures no items
+			// are skipped during the current batch of OnUpdate executions.
 
 			// The number of elements to move is the total count minus the index of the item after the removed one.
 			// Example: count is now 4, we removed index 1. We need to move elements from index 2 up to index 3.
-			Array.Copy(instances, removeAt + 1, instances, removeAt, instanceCount - removeAt);
-			if(currentInstanceIndex > removeAt)
+			Array.Copy(instances, instanceIndex + 1, instances, instanceIndex, instanceCount - instanceIndex);
+			currentInstanceIndex--;
+		}
+		
+		void OnDisableOld()
+		{
+			
+		}
+
+		[Preserve, UsedImplicitly]
+		internal static PlayerLoopSystem.UpdateFunction GetUpdateAllInstancesDelegate() => UpdateAllInstances;
+
+		[Preserve, UsedImplicitly]
+		internal static void UpdateAllInstances()
+		{
+			for(currentInstanceIndex = instanceCount - 1; currentInstanceIndex >= 0; currentInstanceIndex--)
 			{
-				currentInstanceIndex--;
+				Debug.Assert(Application.isPlaying);
+				Debug.Assert(instances[currentInstanceIndex]);
+				instances[currentInstanceIndex].OnUpdate();
+			}
+		}
+
+		protected abstract void OnUpdate();
+	}
+
+	/// <summary>
+	/// Non-generic base class of <see cref="UpdatableBehaviour{T}"/>.
+	/// </summary>
+	/// <remarks>
+	/// You should always derive from the generic version <see cref="UpdatableBehaviour{T}"/> instead of this one directly.
+	/// </remarks>
+	public abstract class UpdatableBehaviour : MonoBehaviour
+	{
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+		static void StartUpdatingAllInstances()
+		{
+			var playerLoop = PlayerLoop.GetCurrentPlayerLoop();
+			var rootSystems = playerLoop.subSystemList;
+			for(var systemIndex = rootSystems.Length - 1; systemIndex >= 0; systemIndex--)
+			{
+				var rootSystem = rootSystems[systemIndex];
+				if(rootSystem.type != typeof(Update))
+				{
+					continue;
+				}
+
+				var oldUpdateSystems = rootSystem.subSystemList;
+				if(Array.FindIndex(oldUpdateSystems, x => x.type == typeof(UpdatableBehaviour)) is not -1)
+				{
+					return;
+				}
+
+				var updatableBehaviourTypes =
+					#if UNITY_EDITOR
+					UnityEditor.TypeCache.GetTypesDerivedFrom<UpdatableBehaviour>()
+						.Where(type => type.BaseType is { IsGenericType: true } baseType && baseType.GetGenericTypeDefinition() == typeof(UpdatableBehaviour<>))
+						.ToArray();
+					#else
+					AppDomain.CurrentDomain.GetAssemblies()
+						.Where(assembly => !assembly.IsDynamic)
+						.SelectMany(assembly => assembly.GetTypes())
+						.Where(type => type.IsSubclassOf(typeof(UpdatableBehaviour)) && type.BaseType is { IsGenericType: true } baseType && baseType.GetGenericTypeDefinition() == typeof(UpdatableBehaviour<>))
+						.ToArray();
+					#endif
+
+				var delegatesToAdd = updatableBehaviourTypes.Length;
+				var oldLength = oldUpdateSystems.Length;
+				var newLength = oldLength + delegatesToAdd;
+				var newUpdateSystems = new PlayerLoopSystem[newLength];
+				Array.Copy(oldUpdateSystems, newUpdateSystems, oldLength);
+
+				for(var typeIndex = 0; typeIndex < delegatesToAdd; typeIndex++)
+				{
+					var updatableBehaviourType = updatableBehaviourTypes[typeIndex];
+					newUpdateSystems[oldLength + typeIndex] = new()
+					{
+						type = updatableBehaviourType,
+						updateDelegate = (PlayerLoopSystem.UpdateFunction)updatableBehaviourType.BaseType.GetMethod("GetUpdateAllInstancesDelegate", BindingFlags.NonPublic | BindingFlags.Static).Invoke(null, null)
+					};
+				}
+
+				rootSystem.subSystemList = newUpdateSystems;
+				rootSystems[systemIndex] = rootSystem;
+				playerLoop.subSystemList = rootSystems;
+				PlayerLoop.SetPlayerLoop(playerLoop);
+				return;
 			}
 		}
 	}
